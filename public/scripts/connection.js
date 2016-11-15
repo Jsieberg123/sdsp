2100ef2d4b2b488f5ebe2265564877edb80cd1ee
Jacob <jsieberg123@gmail.com>
2016-11-14 20:04:36 -0600

    force sockets to ssl

--- public/scripts/connection.js
+++ public/scripts/connection.js
@@ -27,12 +27,7 @@ ConnectToTopic("test", onMessage, function(connection){connection.send("Hello Wo
 
 function getUri(){
   var loc = window.location, new_uri;
-  if(loc.protocol === "https")  {
-    new_uri = "wss:";
-  }
-  else {
-    new_uri = "ws:";
-  }
+  new_uri = "wss:";
   new_uri += "//"+ loc.host + "/sockets";
   return new_uri;
 }
