81819f33394ce6e212c4cb7eb7b957a8548f4125
Jsieberg123 <jsieberg123@gmail.com>
2016-11-15 22:23:20 -0600

    fixed devices page

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,32 @@
 module.exports = function(app, stormpath, settings) {
 
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
+var addDevicesIfNotPresent = function(req)
+{
+  if(!("devices" in req.user.customData))
+  {
+    req.user.customData.devices = [];
+    req.user.customData.save(function(err){
+      console.log(err);
+    });
+  }
+}
+
+app.use(allowCrossDomain);
+
     // =====================================
     // Main Page ===========================
     // =====================================
     app.get('/', stormpath.loginRequired, function(req, res) {
+      addDevicesIfNotPresent(req);
         res.render('index.ejs', {
             user : req.user, // get the user out of session and pass to template
             projectName: settings.projectName
@@ -14,6 +37,7 @@ module.exports = function(app, stormpath, settings) {
     // Device Discovery Page ===============
     // =====================================
     app.get('/devices', stormpath.loginRequired, function(req, res) {
+      addDevicesIfNotPresent(req);
         res.render('devices.ejs', {
             user : req.user, // get the user out of session and pass to template
             projectName: settings.projectName
