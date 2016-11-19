ac2fc8b120c33fb083c4e8dcdeabe8ddaf172634
Jacob Sieberg <jsieberg123@gmail.com>
2016-11-18 22:00:42 -0600

    Started rendering of data

--- public/scripts/connection.js
+++ public/scripts/connection.js
@@ -1,33 +1,27 @@
 function ConnectToTopic(topic, messageHandler, onConnect)
 {
   var socket = new WebSocket(getUri()+"?topic="+topic +"-web");
-  socket.onopen = function()
-  {
-    onConnect({
-      send: function(msg){
+  var connection = {
+      Send: function(msg){
         socket.send(JSON.stringify({topic:topic+"-device", payload:msg}));
       },
       topic:topic,
       socket:socket
-    });
+    };
+  socket.onopen = function()
+  {
+    onConnect(connection);
   }
 
   socket.onmessage = function (event){
-    messageHandler(event.data);
+    messageHandler(JSON.parse(event.data), connection);
   };
 }
 
-function onMessage(message)
-{
-  console.log(message);
-}
-
-ConnectToTopic("F30Qj8AfZQIhsk9dDGCPGN8JXLENjiN0", onMessage, function(connection){connection.send({action:"Get-Cards"})})
-
 function getUri(){
   var loc = window.location, new_uri;
-  new_uri = "wss:";
-  new_uri += "//"+ loc.host + "/sockets";
-  //new_uri = "ws://127.0.0.1:5693";
+  //new_uri = "wss:";
+  //new_uri += "//"+ loc.host + "/sockets";
+  new_uri = "ws://127.0.0.1:5693";
   return new_uri;
 }
