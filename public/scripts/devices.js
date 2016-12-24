0d3f68075448d622a13b23f457791aa71e0dbbf5
Jacob Sieberg <jsieberg123@gmail.com>
2016-12-23 23:27:04 -0600

    new style

--- public/scripts/devices.js
+++ public/scripts/devices.js
@@ -1,4 +1,5 @@
-var url = "wss://aggautomation.com/sockets"
+var url = "wss://aggautomation/sockets/"
+var events = {};
 
 for (var i = 0; i < devices.length; i++) {
     connect(i, false);
@@ -8,59 +9,68 @@ function connect(i, silent) {
     var id = devices[i].id;
 
     if (!silent) {
+        events[id] = {};
         $("#" + id + "-spinner").show();
         $("#" + id + "-retry").hide();
     }
 
-    var timer = setTimeout(function(id, i) {
+    devices[i].timer = setTimeout(function(id, i) {
         $("#" + id + "-spinner").hide();
         $("#" + id + "-retry").show();
-        setTimeout(function() { connect(i, true); }, 15000);
+        clearTimeout(devices[i].timer);
+        devices[i].timer = setTimeout(function() { connect(i, true); }, 15000);
     }.bind(null, id, i), 5000);
-    var displayInfo = new WebSocket(url + "?topic=display-" + id);
-    displayInfo.i = i;
 
-    displayInfo.onopen = function() {
-        this.send("Get");
+    var socket = new WebSocket(url + "?topic=" + id);
+    socket.id = id;
+    socket.i = i;
+
+    socket.onopen = function() {
+        console.log("open");
+        send(this.id, "get", "");
     }
 
-    displayInfo.onmessage = function(event) {
-        if (event.data === "Get") {
+    socket.onmessage = function(event) {
+        console.log(event);
+        var message = JSON.parse(event.data);
+
+        if (!("e" in message) || !("p" in message)) {
             return;
         }
 
-        devices[this.i].template = ejs.compile(event.data, {});
-        this.close();
+        clearTimeout(devices[this.i].timer);
+        console.log(message.e);
+
+        if (message.e in events[this.id]) {
+            console.log("here");
+            events[this.id][message.e](message.p);
+        }
     }
 
-    var displayData = new WebSocket(url + "?topic=data-" + id);
-    displayData.i = i;
-
-    displayData.onmessage = function(event) {
-        var data = JSON.parse(event.data);
-        if ("cmd" in data) {
-            return;
-        }
-        data.i = displayData.i;
-        data.id = devices[this.i].id
-        if ("template" in devices[this.i]) {
-            var html = devices[this.i].template(data);
-            $("#" + devices[this.i].id).html(html);
-        }
-    };
-
-    displayData.onclose = function() {
-        connect(this.i, true);
+    socket.onclose = function() {
+        console.log("Lossed connection with " + this.id);
+        clearTimeout(devices[this.i].timer);
+        devices[this.i].timer = setTimeout(function() { connect(i, true); }, 1000);
     }
 
-    devices[i].ws = displayData;
+    events[id]["display"] = function(display) {
+        console.log(id);
+        $("#" + id).html(display);
+    }
+
+    events[id].socket = socket;
 }
 
-function send(i, cmd, payload) {
-    var message = JSON.stringify({
-        cmd: cmd,
-        payload: payload
-    });
+function send(device, event, payload) {
+    var message = {
+        e: event,
+        p: payload
+    }
+    message = JSON.stringify(message);
 
-    devices[i].ws.send(message);
+    events[device].socket.send(message);
+}
+
+function on(device, event, callback) {
+    events[device][event] = callback;
 }
